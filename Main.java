27a51accb9aaaf7a62c6a4bd41f3392679a8d440
DivyanshuSingh43 <singhdivyanhu0408@gmail.com>
2026-01-16 00:01:13 +0530

    First Commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+class Main{
+
+    public static void main(String[] args){
+    
+        String s = "HelloWorld";
+
+      	// Object Initialised
+        StringBuilder res = new StringBuilder();
+
+        // Appending elements of s in res
+        res.append(s);
+
+        // reverse StringBuilder res
+        res.reverse();
+
+        // print reversed String
+        System.out.println(res);
+    }
+}
+
